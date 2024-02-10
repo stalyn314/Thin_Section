3bc7a6ec0a78ea004677792fac5e57f433cebced
stalyn314 <152093310+stalyn314@users.noreply.github.com>
2024-02-10 03:31:51 -0500

    generate_caption.py

--- model/generate_caption.py
+++ model/generate_caption.py
@@ -75,9 +75,8 @@ def generate_caption(file):
 
     decoded_caption = decoded_caption.replace("<start> ", "")
     decoded_caption = decoded_caption.replace(" <end>", "").strip()
-    CAP_caption='. '.join(map(lambda s: s.strip().capitalize(), decoded_caption.split('.')))
-    print("Predicted Caption: ", CAP_caption)
-    return f'Predicted: {CAP_caption}'
+    print("Predicted Caption: ", decoded_caption)
+    return f'Predicted: {decoded_caption}'
 
 # Check predictions for a few samples
 # generate_caption('boat.jpeg')
