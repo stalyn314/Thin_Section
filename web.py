9389eb282022e566557dc767027e7d4a5810d433
stalyn314 <152093310+stalyn314@users.noreply.github.com>
2024-02-10 09:27:06 -0500

    web.py

--- web.py
+++ web.py
@@ -1,1 +1,26 @@
+import streamlit as st
+import requests
+import numpy as np
+from PIL import Image
+from model.generate_caption import *
+import datetime
+import psycopg2
+import pandas as pd
 
+st.title('Descripción automática de secciones delgadas de rocas')
+
+
+uploaded_files = st.file_uploader("Upload some file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    print('Name of the file upload is: ',uploaded_file.name)
+    location = f'Upload/{uploaded_file.name}'
+    with open(os.path.join("Upload",uploaded_file.name),"wb") as f: 
+      f.write(uploaded_file.getbuffer()) 
+    pred_caption = generate_caption(location)
+    print(pred_caption)
+    print(type(bytes_data))
+    st.image(location)
+    st.write(pred_caption)
+    st.write('____________________________________________________________________________________________')
